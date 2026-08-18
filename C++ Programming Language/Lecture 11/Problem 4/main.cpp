af22d6f9083b1b20abff90e66d19b79107a1c94b
SuyashChandraTripathi <suyashchandra9918@gmail.com>
2026-08-18 09:38:40 +0530

    Started new Problem Practice

---
+++ C++ Programming Language/Lecture 11/Problem 4/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cmath>
+using namespace std ;
+int main(){
+
+// Take an odd integer n as input from the user (n≥3). Write a program to print a 'Plus
+// inside Square' Boundary pattern.
+
+}
