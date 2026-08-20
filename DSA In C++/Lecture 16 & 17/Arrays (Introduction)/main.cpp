14162f49a7205cfa2186815eb9f971427ba36e41
SuyashChandraTripathi <suyashchandra9918@gmail.com>
2026-08-20 11:32:12 +0530

    Introduction to Data Structures and Algorithms

---
+++ DSA In C++/Lecture 16 & 17/Arrays (Introduction)/main.cpp
@@ -0,0 +1,53 @@
+// DSA = Data Structures + Algorithms
+
+// Data Structure: How we organize and store data.
+// Algorithm: A step-by-step method to solve a problem.
+
+// For example, if you have 1 million numbers and want to find a particular number:
+// How you store those numbers → Data Structure
+// How you search for the number → Algorithm
+
+// TOPICS 
+
+
+// 1. Complexity Analysis ⭐
+//    A). Time Complexity
+//    B). Space Complexity
+
+// 2. Arrays ⭐⭐⭐
+
+// 3. Strings
+
+// 4. Searching Algorithms ⭐⭐⭐
+
+// 5. Sorting Algorithms ⭐⭐⭐
+
+// 6. Recursion ⭐⭐⭐ 
+
+// 7. Backtracking ⭐⭐⭐
+
+// 8. Linked Lists ⭐⭐⭐
+
+// 9. Stack ⭐⭐⭐
+
+// 10. Queue ⭐⭐⭐
+
+// 11. Hashing ⭐⭐⭐
+
+// 12. Trees ⭐⭐⭐⭐
+
+// 13. Binary Search Tree — BST ⭐⭐⭐⭐
+
+// 14. Heap / Priority Queue ⭐⭐⭐⭐
+
+// 15. Greedy Algorithms ⭐⭐⭐
+
+// 16. Graphs ⭐⭐⭐⭐⭐
+
+// 17. Graph Algorithms ⭐⭐⭐⭐⭐
+
+// 18. Dynamic Programming — DP ⭐⭐⭐⭐⭐
+
+// 19. Tries
+
+// 20. Advanced Data Structures
