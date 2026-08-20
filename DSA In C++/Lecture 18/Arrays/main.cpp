169960ae59668c7b157d021eca6773649b47711f
SuyashChandraTripathi <suyashchandra9918@gmail.com>
2026-08-20 22:17:18 +0530

    Introduction to Array

---
+++ DSA In C++/Lecture 18/Arrays/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <iterator>
+using namespace std ;
+int main (){
+    // int marks[] = {23,65,89,73,45,63} ;
+    
+    // cout << marks[1] << endl ; 
+    
+    // marks[1] = 54 ;
+    // cout << marks[1] << endl ;
+    
+    // cout << "Enter the updated marks: " ;
+    // cin >> marks[1] ;
+    // cout << marks[1] << endl ; ;
+
+// SIZE OF ARRAYS
+     
+    // cout << size(marks) << endl ; //valid only for compiler more updated than C++ 17
+    // cout << sizeof(marks)/sizeof(marks[0]) << endl ;
+
+// OUTPUT ARRAY
+    
+    // int arr[] = {54,33,24,56,8,4,56,78,21,11,99} ;
+    // int n = sizeof(arr) / sizeof(arr[0]) ;
+    // for (int i = 0 ; i <= n-1 ; i++){
+    //     cout << arr[i] << " " ;
+    // }
+
+// GARBAGE VALUE
+
+    // int arr[4] ;
+    // cout << arr[0] ; // Garbage value
+}
