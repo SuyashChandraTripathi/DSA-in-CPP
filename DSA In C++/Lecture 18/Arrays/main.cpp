699492e1f0ac91317f0d1a5ca8716d98e743f4d0
SuyashChandraTripathi <suyashchandra9918@gmail.com>
2026-08-21 16:04:23 +0530

    Updated the Arrays section

--- DSA In C++/Lecture 18/Arrays/main.cpp
+++ DSA In C++/Lecture 18/Arrays/main.cpp
@@ -33,16 +33,42 @@ int main (){
 
 // INPUT
 
-    int n ;
-    cout << "Enter the size of array: " ;
-    cin >> n ;
-    int arr[n] ;
-    cout << "Enter the elements of array: " ;
-    for(int i = 0 ; i <= n-1 ; i++){
-        cin >> arr[i] ;
-    }
-    cout << "The array given as input is: " ;
-    for(int i = 0 ; i <= n-1 ; i++){
-        cout << arr[i] << " " ;
-    }
+    // int n ;
+    // cout << "Enter the size of array: " ;
+    // cin >> n ;
+    // int arr[n] ;
+    // cout << "Enter the elements of array: " ;
+    // for(int i = 0 ; i <= n-1 ; i++){
+    //     cin >> arr[i] ;
+    // }
+    // cout << "The array given as input is: " ;
+    // for(int i = 0 ; i <= n-1 ; i++){
+    //     cout << arr[i] << " " ;
+    // }
+
+// INSUFFICIENT ENTRIES INSIDE AN ARRAY
+    // int arr[10] = {12 , 23 , 55 , 60 , 53} ;
+    // int n = sizeof(arr)/sizeof(arr[0]) ;  
+    // for(int i = 0 ; i <= n-1 ; i++){
+    //     cout << arr[i] << " " ;
+    // }
+
+// EXCESS ENTRIES INSIDE AN ARRAY(WILL GIVE AN ERROR)
+    // int arr[5] = {12 , 15 , 65 , 77 , 32 , 45 , 23} ;
+    // int n = sizeof(arr)/sizeof(arr[0]) ;
+    // for(int i = 0 ; i <= n-1 ; i++){
+    //     cout << arr[i] << " " ;
+    // }
+
+// AVOIDING GARBAGE VALUE IN AN ARRAY
+    // int arr[10] = {} ;
+    // int n = sizeof(arr)/sizeof(arr[0]) ;
+    // for (int i = 0 ; i <= n-1 ; i++){
+    //     cout << arr[i] << " " ;
+    // }
+
+// INDEX OUT OF BOUND
+    // int arr[] = {12 , 34 , 65 , 77 , 53} ;
+    // cout << arr[7] ; // Undefined Behaviour
+    // cout << arr[-1] ; // Undefined Behaviour
 }
--- DSA In C++/Lecture 18/Questions/main.cpp
+++ DSA In C++/Lecture 18/Questions/main.cpp
@@ -3,18 +3,43 @@
 using namespace std ;
 int main(){
 
+
 // Make an array by taking the entries input and print the negative elements of the array.
-    int a ;
-    cout << "Enter the size of the array: ";
-    cin >> a ;
-    int arr[a] ;
-    cout << "Enter the elements of the array: " ;
-    for(int i = 0 ; i <= a-1 ; i++){
-        cin >> arr[i] ;
-    }
-    cout << "The negative element of array is: " ;
-    for (int i = 0 ; i <= a-1 ; i++){
-        if (arr[i] < 0) {cout << arr[i] << " " ;
-        } 
-    }
+    // int a ;
+    // cout << "Enter the size of the array: ";
+    // cin >> a ;
+    // int arr[a] ;
+    // cout << "Enter the elements of the array: " ;
+    // for(int i = 0 ; i <= a-1 ; i++){
+    //     cin >> arr[i] ;
+    // }
+    // cout << "The negative element of array is: " ;
+    // for (int i = 0 ; i <= a-1 ; i++){
+    //     if (arr[i] < 0) {cout << arr[i] << " " ;
+    //     } 
+    // }
+
+
+// Print sum of elements of the array.
+    // int n ;
+    // cout << "Enter the number of elements in the array: " ;
+    // cin >> n ;
+    // int arr[n] ;
+    // cout << "Enter each element of the array: " ;
+    // for(int i = 0 ; i <= n-1 ; i++){
+    //     cin >> arr[i] ;
+    // }
+    // cout << "The array is --> " ;
+    // for(int i = 0 ; i <= n-1 ; i++){
+    //     cout << arr[i] << " ";
+    // }
+    // int sum = 0 ;
+    // cout << "\nThe sum of all the elements in the array is --> " ;
+    // for(int i = arr[0] ; i <= arr[n-1] ; i++){
+    //     sum += i ;
+    // }
+    // cout << sum ;
+
+
+// 
 }
